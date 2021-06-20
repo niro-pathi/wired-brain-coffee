6cfbad82d18802994ad3eea40d1ef295645b2bb2
Capt. Niro <niroshana.pathirana@crownresorts.com.au>
2021-06-20 15:52:39 +1000

    Solution structure added

---
+++ src/WiredBrainCoffee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainCoffee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Wired Brain Coffee..!");
+        }
+    }
+}
